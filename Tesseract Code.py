e1e468fb34685cac30b8e0c5908efadf772b54b3
vchima <142561017+vchima@users.noreply.github.com>
2023-11-18 17:24:37 -0500

    Update Tesseract Code.py

--- Tesseract Code.py
+++ Tesseract Code.py
@@ -4,26 +4,28 @@ import pytesseract
 # Setting the Tesseract executable path
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\nkhan29\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 
-# Loading the image
+# Loading the image for OCR
 filename = '1_python-ocr.jpg'
 img1 = Image.open(filename)
 
 # Performing OCR on the image
-text = pytesseract.image_to_string(img1, lang='eng')  # Language set to English
+text = pytesseract.image_to_string(img1, lang='eng')  # Extracting text from the image
 
 # Printing the extracted text
 print(text)
 
-# Function to check for a specific keyword
+# Function to check for a specific keyword in the extracted text
 def check_keyword(text, keyword):
     return keyword in text
 
-# Define the specific keyword you want to find
+# Defining the specific keyword to search for
 specific_keyword = 'example_keyword'
 
-# Checking for the specific keyword
+# Checking for the specific keyword in the extracted text
 if check_keyword(text, specific_keyword):
     print(f'Found the "{specific_keyword}" keyword!')
 else:
     print(f'"{specific_keyword}" keyword not found.')
 
+# Comment added to reflect an update
+# Improved readability and added a function for keyword checking
